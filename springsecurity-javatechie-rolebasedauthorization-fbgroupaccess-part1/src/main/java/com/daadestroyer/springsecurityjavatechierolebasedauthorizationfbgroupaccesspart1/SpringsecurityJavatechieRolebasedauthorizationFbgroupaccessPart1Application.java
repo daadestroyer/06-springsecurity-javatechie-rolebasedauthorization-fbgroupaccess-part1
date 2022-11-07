1b9e78e720a6b69059ee61b27a8bdddb975aff44
Shubham Nigam <apple@Apples-MacBook-Pro.local>
2022-11-07 13:34:22 +0530

    code started...

---
+++ springsecurity-javatechie-rolebasedauthorization-fbgroupaccess-part1/src/main/java/com/daadestroyer/springsecurityjavatechierolebasedauthorizationfbgroupaccesspart1/SpringsecurityJavatechieRolebasedauthorizationFbgroupaccessPart1Application.java
@@ -0,0 +1,13 @@
+package com.daadestroyer.springsecurityjavatechierolebasedauthorizationfbgroupaccesspart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityJavatechieRolebasedauthorizationFbgroupaccessPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityJavatechieRolebasedauthorizationFbgroupaccessPart1Application.class, args);
+	}
+
+}
---
+++ springsecurity-javatechie-rolebasedauthorization-fbgroupaccess-part1/src/test/java/com/daadestroyer/springsecurityjavatechierolebasedauthorizationfbgroupaccesspart1/SpringsecurityJavatechieRolebasedauthorizationFbgroupaccessPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daadestroyer.springsecurityjavatechierolebasedauthorizationfbgroupaccesspart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityJavatechieRolebasedauthorizationFbgroupaccessPart1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
